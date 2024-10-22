b58f0d866b6a674d6a102b22ad81e2acd79e21f6
Amitkumarsharma7082 <aksharma7404@gmail.com>
2024-10-22 14:28:43 +0530

    first commit

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //declear
+    int score;
+    score = 110;
+
+    int balance = 500;
+
+    cout<<"Welcome to CPP"<< endl;
+    return 0;
+}
---
+++ test/hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+// input output stream
+// Pre processor director > inclucive 
+
+using std :: cout;
+using std :: endl;
+// using namespace std;
+// namespace myChai{
+//     void display(){ 
+//         // comment
+//     }
+// }
+int main(){
+    // myChai::display();
+    cout<<"Hello World!" << endl;
+    return 0;
+}
